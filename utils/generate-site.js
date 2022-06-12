0c2a512d7df06e58f748a52986382901530b44a0
Samantha-Ruth <sam.haberman@gmail.com>
2022-06-12 16:57:46 -0600

    fixed a few issues with code.. mainly a slash in the file source under the copy file function

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,39 +1,37 @@
 const fs = require('fs');
 
-// create 2 functions that return a  promise, one for writing a file and another for copying a file
-
+// writing files
 const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/index.html', fileContent, err => {
-            // if there's an error, reject the promise and send the error to the Promises's '.catch() method
-            if (err) {
-                reject(err)
-                // return out of functiom
-                return;
-            }
-        // if everything went well, resolve the Promise and send successful data to the .then() method
-        resolve ({
-            ok: true,
-            message: 'File created!'
-        });
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
     });
-});
+  });
 };
 
-const copyFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.copyFile('./src/style.css', '.dist/style.css', err => {
-            // if there's an error, reject the promise and send the error to the Promises's '.catch() method
-            if (err) {
-                reject(err)
-                // return out of functiom
-                return;
-            }
-        // if everything went well, resolve the Promise and send successful data to the .then() method
-        resolve ({
-            ok: true,
-            message: 'File created!'
-        });
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'Stylesheet created!'
+      });
     });
-});
+  });
 };
+
+module.exports = { writeFile, copyFile };
