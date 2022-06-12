5ac5772c2b5c98f4bdca9441fda7222ddd3f437d
Samantha-Ruth <sam.haberman@gmail.com>
2022-06-12 16:19:12 -0600

    added first promise, working on second

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -19,3 +19,21 @@ const writeFile = fileContent => {
     });
 });
 };
+
+const copyFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', '.dist/style.css', err => {
+            // if there's an error, reject the promise and send the error to the Promises's '.catch() method
+            if (err) {
+                reject(err)
+                // return out of functiom
+                return;
+            }
+        // if everything went well, resolve the Promise and send successful data to the .then() method
+        resolve ({
+            ok: true,
+            message: 'File created!'
+        });
+    });
+});
+};
